1c3c406bd3f2c37714cab81b293ca3247bf5a0aa
Roman Batrakov <Kyct69@yandex.ru>
2024-02-11 13:25:08 +0300

    Delete nonUse files

--- Test_Project_01/AbstractClass/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"App"},{"p":"<Unnamed>","l":"Circle"},{"p":"<Unnamed>","l":"Square"},{"p":"<Unnamed>","l":"Triangle"}];updateSearchResults();
